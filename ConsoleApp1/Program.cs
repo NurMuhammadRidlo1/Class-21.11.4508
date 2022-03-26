f41ea91478c6a6f2dd156edc9062aeb77db5d572
NurMuhammadRidlo1 <nurmuhammadridlo@gmail.com>
2022-03-26 23:19:07 +0700

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Membuat Object Taxi
+            ConsoleApp1.Taxi taxi = new ConsoleApp1.Taxi();
+
+            // Pengesetan nilai properties
+            taxi.DriverName = "Gugun";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            // Panggil Method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOfPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/Taxi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            Console.WriteLine("OnDuty : {0} ", OnDuty);
+            Console.WriteLine("Number of Passenger : {0}", NumPassenger);
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine(DriverName + " sedang menjemput penumpang!");
+        }
+
+        public void DropOfPassenger()
+        {
+            Console.WriteLine(DriverName + " selesai mengatar penumpang!");
+        }
+    }
+}
